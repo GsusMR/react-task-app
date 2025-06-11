633cd884c198bcebdb3431940a9b22b300480f2d
GsusMR <jmoreno91@alumnos.uaq.mx>
2025-06-11 10:40:35 -0600

    Added tailwind styles

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -19,18 +19,25 @@ function TaskForm() {
     }
   
     return (
-        <form onSubmit={handleSubmit}>
-            <input placeholder="Escribe tu tarea"
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-            autoFocus
-            />
+        <div className="max-w-md mx-auto">
+            <form className="bg-slate-800 p-10 mb-4" onSubmit={handleSubmit}>
+                <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
+                <input 
+                className="bg-slate-300 p-3 w-full mb-2"
+                placeholder="Escribe tu tarea"
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+                autoFocus
+                />
 
-            <textarea placeholder="Descripción tarea"
-            onChange = {(e) => setDescription(e.target.value)}
-            value={description}></textarea>
-            <button>Guardar</button>
+                <textarea 
+                className="bg-slate-300 p-3 w-full mb-2"
+                placeholder="Descripción tarea"
+                onChange = {(e) => setDescription(e.target.value)}
+                value={description}></textarea>
+                <button className="bg-indigo-500 px-3 py-1 text-white hover:bg-indigo-300">Guardar</button>
         </form>   
+        </div>
     )
 }
 
